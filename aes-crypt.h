3c2b85b2df76ab197fc10726d9a7860d8573f2d2
Apoak <apokerlu@calpoly.edu>
2025-06-04 20:28:08 -0700

    up to date

--- aes-crypt.h
+++ aes-crypt.h
@@ -6,10 +6,12 @@
 #include <openssl/evp.h>
 #include <openssl/aes.h>
 #include <openssl/sha.h>
+#include <openssl/rand.h>
 #define BLOCKSIZE 1024
 #define FAILURE 0
 #define SUCCESS 1
-#define IV_SIZE_BYTES 32
+#define IV_SIZE_BYTES 16
+
 /* int do_crypt(FILE* in, FILE* out, int action, char* key_str)
 * Purpose: Perform cipher on in File* and place result in out File*
 * Args: FILE* in : Input File Pointer
@@ -21,4 +23,10 @@
 */
 extern int do_crypt(FILE* in, FILE* out, int action, char* key_str, unsigned char* iv_buffer);
 
+/* int generate_random_iv(unsigned char *iv)
+* Purpose: Generate a random IV for AES-256-CBC and place result in provided IV buffer
+* Args: unsigned char *iv : IV buffer (should be 16 bytes)
+* Return: FAILURE on error, SUCCESS on success
+*/
+extern int generate_random_iv(unsigned char *iv);
 #endif
